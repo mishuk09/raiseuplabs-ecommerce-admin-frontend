22ce82143f97582b9572718c5351032760159f71
mishuk09 <mishukinfo09@gmail.com>
2025-03-16 15:47:23 +0600

    make alers component z-20

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Alert = ({ name }) => {
     return (
-        <div className="fixed top-2 right-2  bg-green-600 rounded text-white font-medium    px-4 py-3">
+        <div className="fixed top-2 right-2 z-30  bg-green-600 rounded text-white font-medium    px-4 py-3">
 
             {name}
         </div>
